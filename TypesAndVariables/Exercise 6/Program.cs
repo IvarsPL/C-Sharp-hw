105903f981e1642162bad7097398b565cc64232c
IvarsPL <plaudisivars@gmail.com>
2021-07-19 12:44:29 +0300

    Types and variables done

---
+++ TypesAndVariables/Exercise 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter digits you wan to sum up:");
+            string result = Console.ReadLine();
+            var intList = result.Select(digit => int.Parse(digit.ToString()));
+
+            int sum = intList.Sum();
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TypesAndVariables/Exercise 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter minutes that need to be calculated:");
+            int.TryParse(s: Console.ReadLine(), out int minutes);
+           
+            int minutesInAYear = 525600;
+            int minutesInADay = 1440;
+
+            int years = minutes / minutesInAYear;
+            int days = (minutes % minutesInAYear) / minutesInADay;
+            Console.WriteLine($"It will be {years} year/-s and {days} day/-s.");
+
+            Console.ReadKey();
+        }
+    }
+}
