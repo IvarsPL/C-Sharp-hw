2350326221a1bd3fe7a75296a920a179b881c227
IvarsPL <plaudisivars@gmail.com>
2021-07-19 12:44:29 +0300

    Types and variables done

---
+++ TypesAndVariables/Exercise 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] courses = {"English III", "Precalculus", "Music Theory", "Biotechnology", "Principles of Technology I", "Latin II", "AP US History", "Business Computer Information Systems"};
+            string[] teachers = { "Ms. Lapan", "Mrs. Gideon", "Mr. Davis", "Ms. Palmer", "Ms. Garcia", "Mrs. Barnett", "Ms. Johannessen", "Mr. James"};
+
+          Console.WriteLine("+------------------------------------------------------------+");
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.Write($"| {i+1} |");
+                Console.Write("{0,38}", courses[i]);
+                Console.Write(" | ");
+                Console.Write("{0,15}", teachers[i]);
+                Console.WriteLine(" |");
+
+            }
+            Console.WriteLine("+------------------------------------------------------------+");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TypesAndVariables/Exercise 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Data");
+            Console.Write("Input distance in meters: ");
+            int.TryParse(s: Console.ReadLine(), out var distance);
+            Console.Write("Input hour: ");
+            int.TryParse(s: Console.ReadLine(), out var hours);
+            Console.Write("Input minutes: ");
+            int.TryParse(s: Console.ReadLine(), out var minutes);
+            Console.Write("Input seconds: ");
+            int.TryParse(s: Console.ReadLine(), out var seconds);
+
+            double mile = 1.60934f;
+            double kilometer = 0.001f;
+
+            double timeInSeconds = minutes * 60 + hours * 3600;
+            double timeInHours = (minutes / 60) + (seconds / 3600) + hours;
+            double metersPerSecond = distance / timeInSeconds;
+            double kilometersPerHour = Math.Round ( (distance * kilometer) / timeInHours, 8);
+            double milesPerHour = Math.Round(kilometersPerHour / mile, 8);
+
+            Console.WriteLine("Expected output :");
+            Console.WriteLine($"Your speed in meters/second is {metersPerSecond}");
+            Console.WriteLine($"Your speed in km/h is {kilometersPerHour}");
+            Console.WriteLine($"Your speed in miles/h is {milesPerHour}");
+
+            Console.ReadKey();
+        }
+    }
+}
