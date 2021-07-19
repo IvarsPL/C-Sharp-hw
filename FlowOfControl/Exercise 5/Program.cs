294e90165eeee6f26e046a142fff7fd5b6c29950
IvarsPL <plaudisivars@gmail.com>
2021-07-19 14:52:47 +0300

    Exercise 4 done

---
+++ FlowOfControl/Exercise 5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your word:");
+            var input = Console.ReadLine().ToLower();
+
+            
+        }
+    }
+}
