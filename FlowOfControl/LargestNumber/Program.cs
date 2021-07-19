8b247147dae6116526f78a8eeaaabec2bd10dfd9
IvarsPL <plaudisivars@gmail.com>
2021-07-19 13:08:25 +0300

    Largest Number done

--- FlowOfControl/LargestNumber/Program.cs
+++ FlowOfControl/LargestNumber/Program.cs
@@ -1,27 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LargestNumber
 {
     class Program
     {
-        //TODO: Write a C# program to to find the largest of three numbers.
         static void Main(string[] args)
         {
-            Console.WriteLine("Input the 1st number: ");
-            var input1 = Console.ReadLine();
+            Console.Write("Input the 1st number: ");
+            var input1 = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            Console.Write("Input the 2nd number: ");
+            var input2 = Int32.Parse(Console.ReadLine());
 
-            Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            Console.Write("Input the 3rd number: ");
+            var input3 = Int32.Parse(Console.ReadLine());
+
+            var largestNumber = new List<int> { input1, input2, input3 }.Max();
+            Console.WriteLine($"Largest number is {largestNumber}");
+
+            Console.ReadKey();
         }
     }
 }
