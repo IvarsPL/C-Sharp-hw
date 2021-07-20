27e00f52cbc51778ade22cec791072eddc6f0f20
IvarsPL <81179301+IvarsPL@users.noreply.github.com>
2021-07-20 13:03:54 +0300

    Update Program.cs

--- TypesAndVariables/Exercise 7/Program.cs
+++ TypesAndVariables/Exercise 7/Program.cs
@@ -22,9 +22,7 @@ namespace Exercise_7
             {
                 char[] stringArray = input.ToCharArray();
                 int result = 0;
-
-               int finalCount = ReturnCount(stringArray,ref result);
-               
+                int finalCount = ReturnCount(stringArray,ref result);
                 Console.WriteLine($"The amount of uppercase letters is {finalCount}");
             }
 
