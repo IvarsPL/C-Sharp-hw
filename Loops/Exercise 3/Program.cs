7227a7ca15d601a8189da3df6a56254715566f40
IvarsPL <plaudisivars@gmail.com>
2021-07-21 16:20:25 +0300

    Exercise 3 done

---
+++ Loops/Exercise 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomArray = new int[20];
+            for (var i = 0; i < randomArray.Length; i++)
+            {
+                Random r = new Random();
+                int randomNumber = r.Next(0, 100);
+                randomArray[i] = randomNumber;
+            }
+
+            Console.WriteLine("Which index of random array you want to access?");
+            var indexNumber = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"The number at your index is {randomArray[indexNumber]}");
+             
+            Console.ReadKey();
+        }
+    }
+}
