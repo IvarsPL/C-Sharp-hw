3eb81b6318b5e46465f72efd92f188beb14095aa
IvarsPL <plaudisivars@gmail.com>
2021-07-21 20:14:39 +0300

    Exercise 9 done

---
+++ Loops/Exercise 9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class RollTwoDice
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Desired sum: ");
+            var desiredSum = Int32.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            var diceSum = 0;
+
+            do
+            {
+                Random r = new Random();
+                int dieOne = r.Next(1, 6);
+                int dieTwo = r.Next(1, 6);
+                diceSum = dieOne + dieTwo;
+                Console.WriteLine($"{dieOne} and {dieTwo} = {diceSum}");
+            } while (desiredSum != diceSum);
+        }
+    }
+}
