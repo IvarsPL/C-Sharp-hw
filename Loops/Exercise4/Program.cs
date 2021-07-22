d72bce64e0784678f9862043d1eafa7ad4a2ccb6
IvarsPL <81179301+IvarsPL@users.noreply.github.com>
2021-07-22 23:41:55 +0300

    Update Program.cs

--- Loops/Exercise4/Program.cs
+++ Loops/Exercise4/Program.cs
@@ -4,7 +4,6 @@ namespace Exercise4
 {
     internal class Program
     {
-        //TODO: print all vowels using for and foreach
         private static void Main(string[] args)
         {
             char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
