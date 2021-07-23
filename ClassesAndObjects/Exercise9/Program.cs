0ad28d3bd97ac61593a07c6ec91393a2bc041e7d
IvarsPL <plaudisivars@gmail.com>
2021-07-23 16:52:04 +0300

    Exercise 9 done

---
+++ ClassesAndObjects/Exercise9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5,2);
+            Point p2 = new Point(-3, 6);
+
+            Console.WriteLine($"({p1.p1},{p1.p2})");
+            Console.WriteLine($"({p2.p1},{p2.p2})");
+            SwapPoint(p1, p2);
+            Console.WriteLine($"({p1.p1},{p1.p2})");
+            Console.WriteLine($"({p2.p1},{p2.p2})");
+        }
+
+        public static void SwapPoint(Point p, Point z)
+        {
+            var useless = p.p1;
+            var uselessTwo = p.p2;
+            var uselessThree = z.p1;
+            var uselessFour = z.p2;
+            p.p1 = uselessThree;
+            p.p2 = uselessFour;
+            z.p1 = useless;
+            z.p2 = uselessTwo;
+
+        }
+    }
+}
---
+++ ClassesAndObjects/Exercise9/SwapPoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise9
+{
+    class Point
+    {
+        public int p1;
+        public int p2;
+
+        public Point(int x, int y)
+        {
+            this.p1 = x;
+            this.p2 = y;
+
+        }
+    }
+}
