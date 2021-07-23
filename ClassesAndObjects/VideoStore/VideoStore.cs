519100f98c0177b24b841bbf6b30e3eb6a9902bb
IvarsPL <plaudisivars@gmail.com>
2021-07-23 20:38:33 +0300

    VideoStore done

--- ClassesAndObjects/VideoStore/VideoStore.cs
+++ ClassesAndObjects/VideoStore/VideoStore.cs
@@ -1,35 +1,59 @@
-﻿namespace VideoStore
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VideoStore
 {
     class VideoStore
     {
-
+        private List<Video> _moviesList;
         public VideoStore()
         {
-
+            _moviesList = new List<Video>();
         }
 
         public void AddVideo(string title)
         {
-
+            _moviesList.Add(new Video(title));
         }
 
         public void Checkout(string title)
         {
-
+            var movie = FindAMovieByTitle(title);
+            movie?.BeingCheckedOut();
+              
         }
 
         public void ReturnVideo(string title)
         {
+            var movie = FindAMovieByTitle(title);
+            movie?.BeingReturned();
         }
 
         public void TakeUsersRating(double rating, string title)
         {
-
+            var movie= FindAMovieByTitle(title);
+                    movie.ReceivingRating(rating);
         }
 
         public void ListInventory()
         {
+            foreach (var movie in _moviesList)
+            {
+                Console.WriteLine(movie);
+            }
+        }
 
+        private Video FindAMovieByTitle(string title)
+        {
+            foreach (var movie in _moviesList)
+            {
+                if (movie.Title != title) continue;
+                {
+                    return movie;
+                }
+            }
+            return null;
         }
     }
 }
