1a6b185199ba8bbbb0965f64399bef8e8d2bca34
IvarsPL <plaudisivars@gmail.com>
2021-07-23 14:44:00 +0300

    Exercise 7 done

---
+++ ClassesAndObjects/Exercise7/Dog.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise7
+{
+    class Dog
+    {
+        public string Name;
+        public string Sex;
+        public string Father = "";
+        public string Mother = "";
+
+        public Dog(string name, string sex)
+        {
+            this.Name = name;
+            this.Sex = sex;
+        }
+
+        public Dog(string name, string sex, string father, string mother)
+        {
+            this.Name = name;
+            this.Sex = sex;
+            this.Father = father;
+            this.Mother = mother;
+        }
+
+        public string FathersName()
+        {
+            return this.Father switch
+            {
+                "" => "Unknown",
+                _ => this.Father
+            };
+        }
+
+        public bool HasSameMotherAs(Dog otherDog)
+        {
+            if (otherDog.Mother == this.Mother) return true;
+            return false;
+        }
+
+
+    }
+}
