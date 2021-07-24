3e2ccae57fd5e5d5b0800f44a86cc7a4cb596c6d
IvarsPL <plaudisivars@gmail.com>
2021-07-24 19:01:46 +0300

    Exercise 3 done

---
+++ Collections/Exercise3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "_";
+            var list = new List<string>();
+            do
+            {
+                Console.Write("Enter name: ");
+                name = Console.ReadLine();
+                if (!list.Contains(name)) list.Add(name);
+
+            } while (!string.IsNullOrEmpty(name));
+
+            foreach (var person in list) { Console.WriteLine(person); }
+
+            Console.Read();
+        }
+    }
+}
