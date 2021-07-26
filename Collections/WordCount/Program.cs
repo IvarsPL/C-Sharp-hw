7694a65ba093f59b113330f94f128407cf3facd2
IvarsPL <plaudisivars@gmail.com>
2021-07-26 11:42:52 +0300

    Exercise 5 done

--- Collections/WordCount/Program.cs
+++ Collections/WordCount/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,34 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = File.ReadAllText(@"C:\Users\Better\source\repos\C-Sharp-hw\Collections\WordCount\lear.txt");     
+            Console.WriteLine(text);
+            var wordCount = 0;
+            int charCount = 0;
+            int index = 0;
+            var lineCount = File.ReadLines(@"C:\Users\Better\source\repos\C-Sharp-hw\Collections\WordCount\lear.txt").Count();
+            
+            foreach (var c in text)
+            {
+                charCount++;
+            }
+
+            while (index < text.Length)
+            {
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            Console.WriteLine($"Lines = {lineCount}");
+            Console.WriteLine($"Words = {wordCount}");
+            Console.WriteLine($"Chars = {charCount}");
+
+            Console.Read();
         }
     }
 }
