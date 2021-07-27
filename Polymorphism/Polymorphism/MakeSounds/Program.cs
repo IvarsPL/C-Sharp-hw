475a52cd7985b403d810f046c9ea1d0f209d9319
IvarsPL <plaudisivars@gmail.com>
2021-07-27 16:03:32 +0300

    MakeSounds done

--- Polymorphism/Polymorphism/MakeSounds/Program.cs
+++ Polymorphism/Polymorphism/MakeSounds/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MakeSounds
 {
@@ -6,7 +7,24 @@ namespace MakeSounds
     {
         private static void Main(string[] args)
         {
-            
+            var blueParrot = new Parrot();
+            var greenParrot = new Parrot();
+            var oneRadio = new Radio();
+            var smallRadio = new Radio();
+            var loudFirework = new Firework();
+            var bigFirework = new Firework();
+
+            var soundList = new List<ISound>()
+            {
+                blueParrot, greenParrot, oneRadio, smallRadio, loudFirework, bigFirework
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                foreach (var sound in soundList) { sound.PlaySound(); }
+            }
+
+            Console.Read();
         }
     }
 }
