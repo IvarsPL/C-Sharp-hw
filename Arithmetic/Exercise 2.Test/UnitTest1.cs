952db76d1ce1e8e0ef18835dacc46ed518efe48e
IvarsPL <plaudisivars@gmail.com>
2021-07-30 15:45:20 +0300

    Arithmetics Ex 2 done

---
+++ Arithmetic/Exercise 2.Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using Exercise_2;
+
+namespace Exercise_2.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void OddOrEven_EvenNumber_ShouldReturnTrue()
+        {
+            //Arrange
+            var expected = "Even";
+            //Act
+            var actual = Exercise2.OddOrEven( 12);
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void OddOrEven_OddNumber_ShouldReturnTrue()
+        {
+            //Arrange
+            var expected = "Odd";
+            //Act
+            var actual = Exercise2.OddOrEven(111);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Arithmetic/Exercise 2/Exercise2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    public class Exercise2
+    {
+        public static string OddOrEven(int input)
+        {
+           return input % 2 == 0 ? "Even" : "Odd";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter odd or even number:");
+            var userInput = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+            Console.WriteLine(OddOrEven(userInput));
+            Console.WriteLine("Bye!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
