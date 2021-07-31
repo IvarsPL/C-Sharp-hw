2c16eae374e31b58c885d904a440c480dab79077
IvarsPL <plaudisivars@gmail.com>
2021-07-31 16:03:12 +0300

    WordCountTest done

---
+++ Collections/WordCount.Test/WordCount.Test.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace WordCount.Test
+{
+    public class Tests
+    {
+       [Test]
+        public void CharCounter_CheckingNormalChars_ShouldReturnTrue()
+        {
+           //Arrange
+           var text =
+               "Poor naked wretches, wheresoe'er you are,That bide the pelting of this pitiless storm, How shall your houseless heads and unfed sides, Your loop'd and window'd raggedness, defend you From seasons such as these ?  O, I have ta'en Too little care of this!";
+           var expected = 198;
+           //Act
+          var actual = WordCount.CharCounter(text);
+           //Assert
+           Assert.AreEqual(expected,actual);
+        }
+
+        [Test]
+        public void WordCounter_CountingWords_ShouldReturnTrue()
+        {
+            //Arrange
+            var text =
+                "Poor naked wretches, wheresoe'er you are,That bide the pelting of this pitiless storm, How shall your houseless heads and unfed sides, Your loop'd and window'd raggedness, defend you From seasons such as these ?  O, I have ta'en Too little care of this!";
+            var expected = 43;
+            //Act
+            var actual = WordCount.WordCouner(text);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Collections/WordCount/WordCount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        public static int CharCounter(string text)
+        {
+            var charCount = 0;
+            foreach (var c in text)
+            {
+                if(Char.IsLetterOrDigit(c)) charCount++;
+            }
+
+            return charCount;
+        }
+
+        public static int WordCouner(string text)
+        {
+            var wordCount = 0;
+            var index = 0;
+
+            while (index < text.Length)
+            {
+                while (index < text.Length && !char.IsWhiteSpace(text[index]))
+                    index++;
+
+                wordCount++;
+
+                while (index < text.Length && char.IsWhiteSpace(text[index]))
+                    index++;
+            }
+
+            return wordCount;
+        }
+
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\Better\source\repos\C-Sharp-hw\Collections\WordCount\lear.txt");
+            var wordCount = WordCouner(text);
+            int charCount = CharCounter(text);
+            var lineCount = File.ReadLines(@"C:\Users\Better\source\repos\C-Sharp-hw\Collections\WordCount\lear.txt").Count();
+
+            Console.WriteLine($"Lines = {lineCount}");
+            Console.WriteLine($"Words = {wordCount}");
+            Console.WriteLine($"Chars = {charCount}");
+
+            Console.Read();
+        }
+    }
+}
